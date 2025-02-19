4454abbc0ecb0d6256944ebcf4061ca3f67fd6a9
Jake <jacobatienza14@gmail.com>
2025-02-19 02:08:13 -0500

    Removed Vite and React SVG's

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
 
 	return (
 		<>
-			<div></div>
-			<h1>React</h1>
+			<h1>Vite + React</h1>
 			<div className='card'>
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
