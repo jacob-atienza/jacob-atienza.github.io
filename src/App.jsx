b07713e97929a6680587b81c513e8c16bf3520c2
jacob-atienza <jacobatienza14@gmail.com>
2025-03-14 18:46:25 -0400

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Jacob from './Components/Jacob';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Education from './Components/Education';
-import Work from './Components/Work';
-import Contact from './Components/Contact';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Jacob from "./Components/Jacob";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Education from "./Components/Education";
+import Work from "./Components/Work";
+import Contact from "./Components/Contact";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="bg-neutral-800">
+      <Analytics />
       <Navbar />
       <Jacob />
       <About />
